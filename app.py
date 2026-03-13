39f6cc3bccab2b87431d4ff2936c77473104c532
Dhanush292006 <dhanushs99777@gmail.com>
2026-03-13 20:05:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,12 +1,17 @@
 import streamlit as st
+import pandas as pd
 from transformers import pipeline
 
-st.title("AI Chatbot")
+st.title("CSV LLM Chatbot")
 
-chatbot = pipeline("text-generation", model="microsoft/DialoGPT-medium")
+# Load dataset
+df = pd.read_csv("GPT DATA COLLECTION(Sheet1).csv")
 
-prompt = st.text_input("Ask something")
+# Load model
+chatbot = pipeline("text-generation", model="gpt2")
 
-if prompt:
-    result = chatbot(prompt, max_length=100)
-    st.write(result[0]["generated_text"])
+user_input = st.text_input("Ask a question")
+
+if user_input:
+    response = chatbot(user_input, max_length=100)
+    st.write(response[0]['generated_text'])
